f2ff2943031797c873618d6dadf05190034542d3
Jamer Catalla <mr.catalla28@gmail.com>
2025-03-13 23:24:46 +0800

    Added function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  return <DateCounter />;
+}
+
+function DateCounter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+  const date = new Date(); // gets current date
+  date.setDate(date.getDate() + count);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setStep((s) => s - 1)}>-</button>
+      <span>Step: {Math.abs(step)}</span>
+      <button onClick={() => setStep((s) => s + 1)}>+</button>
+      <br />
+      <button onClick={() => setCount((c) => c - step)}>-</button>
+      <span>Count: {Math.abs(count)}</span>
+      <button onClick={() => setCount((c) => c + step)}>+</button>
+      <br />
+      <p>
+        <span>
+          {count === 0
+            ? " Today is : "
+            : count > 0
+            ? `${count} days from today is`
+            : `${Math.abs(count)} days ago was`}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
     </div>
   );
 }
-
 export default App;
