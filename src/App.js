db0b52d81c36bf0cb950f9de855d812679f912aa
Jamer Catalla <mr.catalla28@gmail.com>
2025-03-16 14:58:16 +0800

    Finished challege of Date Counter v1 & v2using useState, Forms Input, & Buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { useState } from "react";
+import "./styles.css";
 
-function App() {
-  return <DateCounter />;
+export default function App() {
+  return (
+    <div className="App">
+      <DateCounter />
+    </div>
+  );
 }
 
 function DateCounter() {
@@ -10,14 +15,32 @@ function DateCounter() {
   const date = new Date(); // gets current date
   date.setDate(date.getDate() + count);
 
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
   return (
     <div>
-      <button onClick={() => setStep((s) => s - 1)}>-</button>
+      <input
+        type="range"
+        min="0"
+        max="10"
+        value={step}
+        onChange={(e) => setStep(Number(e.target.value))}
+      />
       <span>Step: {Math.abs(step)}</span>
-      <button onClick={() => setStep((s) => s + 1)}>+</button>
+      {/* <button onClick={() => setStep((s) => s - 1)}>-</button> */}
+      {/* <span>Step: {Math.abs(step)}</span> */}
+      {/* <button onClick={() => setStep((s) => s + 1)}>+</button> */}
       <br />
       <button onClick={() => setCount((c) => c - step)}>-</button>
-      <span>Count: {Math.abs(count)}</span>
+      <input
+        type="text"
+        value={count}
+        onChange={(e) => setCount(Number(e.target.value))}
+      />
+      {/* <span>Count: {Math.abs(count)}</span> */}
       <button onClick={() => setCount((c) => c + step)}>+</button>
       <br />
       <p>
@@ -25,12 +48,16 @@ function DateCounter() {
           {count === 0
             ? " Today is : "
             : count > 0
-            ? `${count} days from today is`
-            : `${Math.abs(count)} days ago was`}
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
         </span>
         <span>{date.toDateString()}</span>
       </p>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={() => handleReset()}>Reset</button>
+        </div>
+      ) : null}
     </div>
   );
 }
-export default App;
